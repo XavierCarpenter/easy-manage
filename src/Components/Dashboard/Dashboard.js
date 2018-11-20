4aa772eb36bdfb6e0df1d34d8b4d805e3a0d4cd7
Xavier Carpenter <xavier.carpenter16@gmail.com>
2018-11-19 22:23:54 -0600

    dashboard updates

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -13,7 +13,22 @@ class Dashboard extends Component {
                         <li>Notes</li>
                     </ul>
                     </nav>
-              <h1>Dashboard</h1>
+                    <div className="Dashboard-content">
+                    <div className="left-side">
+                    <div className="manager-info">
+                    <h1>Dashboard</h1>
+                    </div>
+                <div className="team-preview">
+                    <h1>My Team</h1>
+                </div>
+                </div>
+                <div className="right-side">
+                <div className="tasks">Tasks</div>
+                <div className="main-content">main-content</div>
+                
+                </div>
+                </div>
+             
             </div>
         );
     }
